80fda0b62b814296bc525c2a944e9cfbf87dbcb5
Edgar <gonzagon111@gmail.com>
2024-06-06 06:24:49 -0400

    Ultimo commit

--- SistemaDeVentas/obj/Debug/net8.0-windows/SistemaDeVentas.AssemblyInfo.cs
+++ SistemaDeVentas/obj/Debug/net8.0-windows/SistemaDeVentas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeVentas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5731c7480f1c2503bd9dcfc038515bfecacfc82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc6a7d9c16ea1ea18866142ac4127f2d7e1cd686")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeVentas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeVentas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
